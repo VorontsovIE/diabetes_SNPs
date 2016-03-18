28f2318a5d325ad37f0b710e50457251dde47762
VorontsovIE <vorontsov.i.e@gmail.com>
2016-03-18 04:33:16 +0300

    filter SNPs not only from entire list

--- filter_SNPs.rb
+++ filter_SNPs.rb
@@ -1,10 +1,11 @@
-All_SNPs_VCF = 'source_data/AllSNPs.vcf'
+all_SNPs_VCF = 'source_data/AllSNPs.vcf'
 
-def filter_SNPs(filename)
+def filter_SNPs(filename, all_SNPs_VCF)
   awk_cmd = 'BEGIN {print "^#"}; {print "\\\\b"$1"\\\\b"}'
-  %x{ cat '#{filename}' | awk '#{awk_cmd}' | egrep  -f - #{All_SNPs_VCF} }
+  %x{ cat '#{filename}' | awk '#{awk_cmd}' | egrep  -f - #{all_SNPs_VCF} }
 end
 
 raise 'Specify file with SNP names (they should be in first column)'  unless filename = ARGV[0]
+all_SNPs_VCF = ARGV[1]  if ARGV[1]
 
-puts filter_SNPs(filename)
+puts filter_SNPs(filename, all_SNPs_VCF)
